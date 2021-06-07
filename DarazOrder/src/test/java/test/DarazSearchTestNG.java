5dd4f9e8629c831377acdcd37723d3e9775f2c1b
mwickramanayaka <manulwick@gmail.com>
2021-06-07 23:20:06 +0530

    TestNG configuration

--- DarazOrder/src/test/java/test/DarazSearchTestNG.java
+++ DarazOrder/src/test/java/test/DarazSearchTestNG.java
@@ -1,46 +1,64 @@
 package test;
-
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
-import Pages.GoogleSearchPageObjects;
 import io.github.bonigarcia.wdm.WebDriverManager;
-
 public class DarazSearchTestNG {
-	
-	private static WebDriver driver = null;
-	
-	@BeforeTest
-	public void setUp() {
-		WebDriverManager.chromedriver().setup();
-	    driver = new ChromeDriver();
-		
-	}
-	
-	@Test
-	public void googleSearchTest() throws InterruptedException{
-	    
-	    GoogleSearchPageObjects searchPageObj = new GoogleSearchPageObjects(driver);
-	    
-	    driver.get("https://www.ebay.com/");
-	    Thread.sleep(1000);
-	    
-	    searchPageObj.setTextInSearchBox("mobile");
-	    Thread.sleep(1000);
+ 
+ WebDriver driver = null;
+ static String expectedTitleebay ="Electronics, Cars, Fashion, Collectibles & More | eBay";
+ static String expectedTitledaraz ="Online Shopping Sri Lanka: Clothes, Electronics & Phones | Daraz.lk";
+ 
+ @BeforeTest
+ public void setUp(){
+	 WebDriverManager.chromedriver().setup();
+	 driver = new ChromeDriver();
+	 
+}
+@Test
+ public void test1() throws InterruptedException {
+ 
+  //maximize the window 
+  driver.manage().window().maximize();
+  driver.get("https://www.ebay.com");
+  Thread.sleep(1000);
+  
+  String actualTitle = driver.getTitle();
+  Assert.assertEquals(actualTitle, expectedTitleebay);
+  
+  driver.findElement(By.xpath("//*[@id=\"gh-ac\"]")).sendKeys("Mobile");
+  Thread.sleep(1000);
+  driver.findElement(By.xpath("//*[@id=\"gh-btn\"]")).sendKeys(Keys.RETURN);
+  Thread.sleep(1000);
+  
+ }
 
-	    searchPageObj.clickSearchButton();
-	    Thread.sleep(1000);
-	    
-	}
-	
-	@AfterTest
-	public void tearDownTest() {
-		
-		driver.close();
-		
-	}
-	
+@Test
+public void test12() throws InterruptedException {
+
+ //maximize the window 
+ driver.manage().window().maximize();
+ driver.get("https://www.ebay.com");
+ Thread.sleep(1000);
+ 
+ String actualTitle = driver.getTitle();
+ Assert.assertEquals(actualTitle, expectedTitleebay);
+ 
+ driver.findElement(By.xpath("//*[@id=\"gh-ac\"]")).sendKeys("Mobile");
+ Thread.sleep(1000);
+ driver.findElement(By.xpath("//*[@id=\"gh-btn\"]")).sendKeys(Keys.RETURN);
+ Thread.sleep(1000);
+ 
+}
+ 
+ @AfterTest
+ public void  tearDown() {
+  driver.quit();
+}
 }
